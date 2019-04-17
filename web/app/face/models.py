76ae3da667aaba70d96a0f1fbc1a802aa4179c5e
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-17 11:09:26 +0800

    added model and form

--- web/app/face/models.py
+++ web/app/face/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class Url(models.Model):
+    image_url = models.CharField(max_length=255)
--- web/app/face/views.py
+++ web/app/face/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from . import models
 
 
 def index(request):
     if request.method == 'POST':
         print(request.POST.get('image_url', ''))
-    context = {'data': 'this is face app'}
+
+        url = models.Url.objects.create(
+            image_url=request.POST.get('image_url', ''))
+    
+        url.save()
+
+
+    urls = models.Url.objects.all()
+
+    context = {'image_urls': urls}
     return render(request, 'face/index.html', context)
