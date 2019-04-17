d6a22b029ac2d68b2b6bd35b986348af52a48958
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-17 13:41:40 +0800

    finished face recognition

--- web/app/face/admin.py
+++ web/app/face/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Url)
+admin.site.register(models.BoundingBox)
--- web/app/face/models.py
+++ web/app/face/models.py
@@ -3,3 +3,11 @@ from django.db import models
 
 class Url(models.Model):
     image_url = models.CharField(max_length=255)
+
+
+class BoundingBox(models.Model):
+    top = models.DecimalField(decimal_places=3, max_digits=5)
+    right = models.DecimalField(decimal_places=3, max_digits=5)
+    bottom = models.DecimalField(decimal_places=3, max_digits=5)
+    left = models.DecimalField(decimal_places=3, max_digits=5)
+    image = models.ForeignKey(Url, on_delete='CASCADE')
