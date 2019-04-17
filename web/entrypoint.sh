667544aa730f1afe116c645befdfa71f7bccd667
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-17 09:47:24 +0800

    setup django and db

---
+++ web/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+echo "run migrations and static"
+python manage.py migrate
+# python manage.py collectstatic --no-input
+
+exec "$@"
