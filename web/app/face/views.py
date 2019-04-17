b2e37058413f4b52b1f642fedb8f9642b9c3e365
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-17 12:39:32 +0800

    added css

--- web/app/face/views.py
+++ web/app/face/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 
 def index(request):
     if request.method == 'POST':
-        print(request.POST.get('image_url', ''))
 
         url = models.Url.objects.create(
             image_url=request.POST.get('image_url', ''))
-    
+
         url.save()
 
+        return redirect('/face')
 
     urls = models.Url.objects.all()
 
