b5c983c6eb4a672e53b61c52a16ba2c87e6bc801
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-04-18 13:41:01 +0800

    added tests

--- web/app/face/test/test_face.py
+++ web/app/face/test/test_face.py
@@ -13,12 +13,12 @@ class FaceTest(TestCase):
     def test_delete_item(self):
         """Test deleting Url with bounding box"""
         url = Url.objects.create(image_url='http://www.gleb.ru/picture.jpg')
-        bb = BoundingBox.objects.create(top=1, bottom=1, right=1, left=1, image=url)
+        BoundingBox.objects.create(top=1, bottom=1, right=1, left=1, image=url)
 
         urls = Url.objects.all()
-        bbs = BoundingBox.objects.all()       
+        bbs = BoundingBox.objects.all()     
 
-        res = self.client.get(reverse('delete', kwargs={'url_id': url.id}))
+        self.client.get(reverse('delete', kwargs={'url_id': url.id}))
         urls = Url.objects.all()
         bbs = BoundingBox.objects.all()
 
