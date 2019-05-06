d430b8654082a43026c42e90e85faf1b0e4c5ac3
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-05-06 16:17:03 +0300

    fixed reset password url

--- web/app/app/urls.py
+++ web/app/app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     # path('users/', include('users.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    # enables reset_password, you can see reset email in logs
+    path('', include('django.contrib.auth.urls')),
 ]
